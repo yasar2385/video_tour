9d069007a5729981e861b65459a5c6e6156f4470
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-09-02 11:30:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# video_tour
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-88hhc5)
---
+++ index.js
@@ -0,0 +1,52 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+document.querySelector('video').onended = function () {
+  if (this.played.end(0) - this.played.start(0) === this.duration) {
+    console.log('Played all');
+  } else {
+    console.log('Some parts were skipped');
+  }
+};
+const $video = document.querySelector('video');
+
+var local_session_time = new Date().getTime();
+var videoTour = {};
+console.log(local_session_time);
+const onTimeUpdate = (event) => {
+  console.log(checkSkipped(event.target.currentTime));
+};
+const onRecordUpdate = (event) => {
+  console.log(event);
+};
+let prevTime = 0;
+const checkSkipped = (currentTime) => {
+  const skip = [];
+  // only record when user skip more than 2 seconds
+  const skipThreshold = 2;
+
+  // user skipped part of the video
+  if (currentTime - prevTime > skipThreshold) {
+    skip.push({
+      periodSkipped: currentTime - prevTime,
+      startAt: prevTime,
+      endAt: currentTime,
+    });
+    prevTime = currentTime;
+    return skip;
+  }
+
+  prevTime = currentTime;
+  return false;
+};
+
+$video.addEventListener("play", e => onRecordUpdate); // console.log('play')
+$video.addEventListener("playing", e => onRecordUpdate); //console.log('playing')
+
+$video.addEventListener("timeupdate", onTimeUpdate);
+
+$video.addEventListener("ended", e => onRecordUpdate);//console.log('ended')
+$video.addEventListener("pause", e => onRecordUpdate); //console.log('pause')
